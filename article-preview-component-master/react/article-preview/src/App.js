b00b5ba1d515f62c61baf9b2c01a0daa5b9ba2fe
Raskolnikov <jerry.tann@opteyes.cn>
2021-08-01 18:22:49 +0800

    feat: test out material media queries

--- article-preview-component-master/react/article-preview/src/App.js
+++ article-preview-component-master/react/article-preview/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 function App() {
+  /* styles */
+  const appStyle = useMediaQuery('(min-width:600px)') ? 'd-app' : 'm-app'
   return (
-    <div className="App">
-      
+    <div className={appStyle}>
+      TESTING
     </div>
   );
 }
