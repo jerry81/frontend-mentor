9152dc9ca50992f9ef5457a8969a5ce401f185ea
Raskolnikov <jerry.tann@opteyes.cn>
2021-08-01 18:43:14 +0800

    feat: start laying out panels

--- article-preview-component-master/react/article-preview/src/App.js
+++ article-preview-component-master/react/article-preview/src/App.js
@@ -1,15 +1,29 @@
-
-import './App.css';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import "./App.css";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 function App() {
   /* styles */
-  const appStyle = useMediaQuery('(min-width:600px)') ? 'd-app' : 'm-app'
-  return (
-    <div className={appStyle}>
-      TESTING
+  const appStyle = useMediaQuery("(min-width:600px)")
+    ? "app d-app"
+    : "app m-app";
+  const innerPanel = useMediaQuery("(min-width:600px)")
+  ? "inner d-inner"
+  : "inner m-inner";
+  /* widgets */
+  function boldText() {
+    return (
+      <div>
+        Shift the overall look and feel by adding these wonderful touches to
+        furniture in your home
+      </div>
+    );
+  }
+  /* render */
+  return <div className={appStyle}>
+    <div className={innerPanel}>
+
     </div>
-  );
+  </div>;
 }
 
 export default App;
