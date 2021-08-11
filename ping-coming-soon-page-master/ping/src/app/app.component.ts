ac4a47d1f35f1143cce4fa17513dc34cab81abb3
Raskolnikov <jerry.tann@opteyes.cn>
2021-08-11 11:08:26 +0800

    feat: mobile active styles bindings

--- ping-coming-soon-page-master/ping/src/app/app.component.ts
+++ ping-coming-soon-page-master/ping/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ping';
+  activeButtons = { fb: false, ig: false, twit: false }
+  error: Boolean = false;
+    
+  toggleError() {
+        this.error = !this.error
+    }
+
+  iconClick(ev) {
+    console.log('ev cur and tar', ev.currentTarget, ev.target)
+    const targetId = ev.target.id
+    if (targetId) {
+      this.activeButtons[targetId] = !this.activeButtons[targetId]
+    }
+  }
 }
+
